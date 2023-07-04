b32e3164764270a2d7a2b8647766179d5fce781b
Seth Obukwelu <138536189+Data-Seth@users.noreply.github.com>
2023-07-04 14:10:44 +0100

    AddAnsys

--- Danny's Diner SQLQuery.sql
+++ Danny's Diner SQLQuery.sql
@@ -1,3 +1,5 @@
+--Displaying the table values
+
 SELECT *
 FROM sales;
 
@@ -57,13 +59,13 @@ FETCH NEXT 1 ROW ONLY;
 
 --Which item was the most popular for each customer?
 
-SELECT S.customer_id, product_name, (S.product_id) 
+SELECT S.customer_id, product_name, (S.product_id)
 FROM sales S
 JOIN menu M
 ON S.product_id = M.product_id
 FULL JOIN members ME
 ON S.customer_id = ME.customer_id
-GROUP BY product_name, S.customer_id
+GROUP BY product_name, S.customer_id;
 
 --Which item was purchased first by the customer after they became a member?
 
@@ -83,10 +85,71 @@ WHERE ROW_NUM = '1';
 
 --Which item was purchased just before the customer became a member?
 
-SELECT *
+WITH LAST_MEM AS
+(
+SELECT S.customer_id, order_date, product_name, join_date
+, MAX(order_date) OVER(PARTITION BY S.customer_id) AS last_order_before_membership
 FROM sales S
 JOIN menu M
 ON S.product_id = M.product_id
 FULL JOIN members ME
 ON S.customer_id = ME.customer_id
---GROUP BY product_name, S.customer_id
+WHERE join_date > order_date
+)
+SELECT customer_id, MAX(last_order_before_membership) AS last_order_before_membership
+FROM LAST_MEM
+GROUP BY customer_id;
+
+--What is the total items and amount spent for each member before they became a member?
+
+SELECT S.customer_id, SUM(price) AS amount_spent_before_membership
+FROM sales S
+JOIN menu M
+ON S.product_id = M.product_id
+FULL JOIN members ME
+ON S.customer_id = ME.customer_id
+WHERE join_date > order_date
+GROUP BY S.customer_id;
+
+--If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+WITH NUMBER_OF_POINTS AS
+(
+SELECT S.customer_id
+,CASE
+	WHEN product_name = 'sushi' THEN price * 20
+	ELSE price * 10
+END AS number_of_points
+FROM sales S
+JOIN menu M
+ON S.product_id = M.product_id
+FULL JOIN members ME
+ON S.customer_id = ME.customer_id
+)
+SELECT customer_id, SUM(number_of_points) AS number_of_points
+FROM NUMBER_OF_POINTS
+GROUP BY customer_id;
+
+--In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi -
+--how many points do customer A and B have at the end of January?
+
+WITH NUMBER_OF_POINTS AS
+(
+SELECT S.customer_id, S.order_date, price
+,CASE
+	WHEN DAY(order_date) BETWEEN DAY(join_date) AND DAY(join_date) + 7 THEN price * 20
+	ELSE price * 10
+END AS number_of_points
+FROM sales S
+JOIN menu M
+ON S.product_id = M.product_id
+FULL JOIN members ME
+ON S.customer_id = ME.customer_id
+WHERE order_date < '2021-01-31'
+)
+SELECT customer_id, SUM(number_of_points) AS number_of_points
+FROM NUMBER_OF_POINTS
+GROUP BY customer_id
+ORDER BY customer_id DESC
+OFFSET 1 ROW
+FETCH NEXT 2 ROW ONLY;
